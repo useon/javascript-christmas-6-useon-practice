e085cf8d0e23d4e785abb0ac53658c26f94192cb
useon <yoosk4986@naver.com>
2023-12-11 17:30:05 +0900

    feat: 12월 이벤트 배지를 출력하는 기능

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -49,6 +49,11 @@ const OutputView = {
     Console.print('\n<할인 후 예상 결제 금액>');
     Console.print(`${amount.toLocaleString()}원`);
   },
+
+  printBadge(name) {
+    Console.print('\n<12월 이벤트 배지>');
+    Console.print(name);
+  },
 };
 
 export default OutputView;
