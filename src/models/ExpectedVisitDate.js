07e40ccc6f2948173e705838f745c7f1e7db689f
useon <yoosk4986@naver.com>
2023-12-15 14:44:16 +0900

    refactor: 밖에서 사용하지 않는 내부 동작은 private 메서드로 수정

--- src/models/ExpectedVisitDate.js
+++ src/models/ExpectedVisitDate.js
@@ -3,18 +3,18 @@ class ExpectedVisitDate {
 
   constructor(inputExpectedVisitDate) {
     this.#expectedVisitDate = 0;
-    this.validate(inputExpectedVisitDate);
-    this.convert(inputExpectedVisitDate);
+    this.#validate(inputExpectedVisitDate);
+    this.#convert(inputExpectedVisitDate);
   }
 
-  validate(inputExpectedVisitDate) {
+  #validate(inputExpectedVisitDate) {
     if (Number.isNaN(Number(inputExpectedVisitDate)))
       throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
     if (Number(inputExpectedVisitDate) < 1 || Number(inputExpectedVisitDate) > 31)
       throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
   }
 
-  convert(inputExpectedVisitDate) {
+  #convert(inputExpectedVisitDate) {
     this.#expectedVisitDate = Number(inputExpectedVisitDate);
   }
 
