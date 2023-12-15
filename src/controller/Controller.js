ff6baead5c27de3dc8e5632b36aac0e144b6de18
useon <yoosk4986@naver.com>
2023-12-15 13:43:56 +0900

    refactor: 자잘한 구현 및 다른 곳에서 쓰이지 않는 메서드는 private로 수정

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,3 +1,4 @@
+import Badge from '../models/Badge.js';
 import Discountor from '../models/Discountor.js';
 import ExpectedVisitDate from '../models/ExpectedVisitDate.js';
 import Order from '../models/Order.js';
@@ -20,38 +21,39 @@ class Controller {
 
   async progress() {
     OutputView.printWelcomeMessage();
-    this.#expectedVisitDate = await handlerErrorAndProceed(this.setExpectedVisitDate);
-    this.#order = await handlerErrorAndProceed(this.setOrder);
+    this.#expectedVisitDate = await handlerErrorAndProceed(this.#setExpectedVisitDate);
+    this.#order = await handlerErrorAndProceed(this.#setOrder);
     OutputView.printEventPreviewMessage(this.#expectedVisitDate);
     OutputView.printOrder(this.#order);
     const beforeSaleAmount = Calculator.calculateBeforeSaleAmount(this.#order);
     OutputView.printBeforeSaleAmount(beforeSaleAmount);
     if (beforeSaleAmount >= 10000)
-      this.discountProcess(this.#expectedVisitDate, this.#order, beforeSaleAmount);
-    if (beforeSaleAmount < 10000) this.notDiscountProcess();
+      this.#discountProcess(this.#expectedVisitDate, this.#order, beforeSaleAmount);
+    if (beforeSaleAmount < 10000) this.#notDiscountProcess();
     beforeSaleAmount >= 120000
-      ? this.totalAmount(beforeSaleAmount, this.#totalDiscount - 25000)
-      : this.totalAmount(beforeSaleAmount, this.#totalDiscount);
-    this.giveBadge();
+      ? this.#totalAmount(beforeSaleAmount, this.#totalDiscount - 25000)
+      : this.#totalAmount(beforeSaleAmount, this.#totalDiscount);
+    const badge = new Badge(this.#totalDiscount).result;
+    OutputView.printBadge(badge);
   }
 
-  async setExpectedVisitDate() {
+  async #setExpectedVisitDate() {
     const userInputExpectedVisitDate = await InputView.readExpectedVisitDate();
     return new ExpectedVisitDate(userInputExpectedVisitDate).Date;
   }
 
-  async setOrder() {
+  async #setOrder() {
     const userInputOrder = await InputView.readOrder();
     return new Order(userInputOrder).order;
   }
 
-  notDiscountProcess() {
+  #notDiscountProcess() {
     OutputView.printGiftMenu(false);
     OutputView.printDisCountDetails(new Map());
     OutputView.printTotalDiscount(0);
   }
 
-  discountProcess(expectedVisitDate, order, beforeSaleAmount) {
+  #discountProcess(expectedVisitDate, order, beforeSaleAmount) {
     const [hasGift, disCountDetails, totalDiscount] = new Discountor(
       expectedVisitDate,
       order,
@@ -63,18 +65,9 @@ class Controller {
     OutputView.printTotalDiscount(this.#totalDiscount);
   }
 
-  totalAmount(beforeSaleAmount, totalDiscount) {
+  #totalAmount(beforeSaleAmount, totalDiscount) {
     const totalAmount = Calculator.calculateAfterSaleAmount(beforeSaleAmount, totalDiscount);
     OutputView.printAfterSaleAmount(totalAmount);
-  }
-
-  giveBadge() {
-    if (this.#totalDiscount < 5000) return OutputView.printBadge('없음');
-    if (this.#totalDiscount >= 5000 && this.#totalDiscount < 10000)
-      return OutputView.printBadge('별');
-    if (this.#totalDiscount >= 10000 && this.#totalDiscount < 20000)
-      return OutputView.printBadge('트리');
-    if (this.#totalDiscount >= 20000) return OutputView.printBadge('산타');
   }
 }
 
